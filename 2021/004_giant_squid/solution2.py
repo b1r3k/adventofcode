013810fab962871d85f081b42d4cb18c8dcd7a64
Lukasz Jachym <lukasz.jachym@gmail.com>
2021-12-04 16:11:05 +0100

    day 4/2

---
+++ 2021/004_giant_squid/solution2.py
@@ -0,0 +1,112 @@
+import sys
+from typing import List
+
+
+class Board:
+    def __init__(self, board: List[List[int]]):
+        self._marks = {}
+        self.rows = board
+        self.max_dim = len(board)
+        self.winning = False
+
+    def mark_number(self, number):
+        for row_idx in range(self.max_dim):
+            for col_idx in range(self.max_dim):
+                if number == self.rows[row_idx][col_idx]:
+                    self._marks[row_idx, col_idx] = True
+                    yield row_idx, col_idx
+
+    def _get_column_marks(self, col_idx):
+        for row_idx in range(self.max_dim):
+            yield self._marks.get((row_idx, col_idx), False)
+
+    def _get_row_marks(self, row_idx):
+        for col_idx in range(self.max_dim):
+            yield self._marks.get((row_idx, col_idx), False)
+
+    def is_winning_row(self, row_idx):
+        marks = list(self._get_row_marks(row_idx))
+        self.winning = all(marks)
+        return self.winning
+
+    def is_winning_column(self, col_idx):
+        marks = self._get_column_marks(col_idx)
+        self.winning = all(marks)
+        return self.winning
+
+    def get_unmarked_numbers(self):
+        for row_idx in range(0, len(self.rows[0])):
+            for col_idx in range(0, len(self.rows[0])):
+                if not self._marks.get((row_idx, col_idx), False):
+                    yield self.rows[row_idx][col_idx]
+
+
+def solution(winning_numbers, boards):
+    nice_boards = list(Board(board) for board in boards)
+    for winning_number in winning_numbers:
+        # print('Checking for winning number: ', winning_number)
+        for board_idx, board in enumerate(nice_boards):
+            for has_number in board.mark_number(winning_number):
+                row_idx, col_idx = has_number
+                # print(f'{winning_number} is in row {row_idx} and column {col_idx} on board {board_idx}')
+                has_winning_column = board.is_winning_column(col_idx)
+                has_winning_row = board.is_winning_row(row_idx)
+                if has_winning_column or has_winning_row:
+                    # print(f'Board {board_idx} is a winner! Column: {has_winning_column} Row: {has_winning_row}')
+                    unmarked_sum = sum(board.get_unmarked_numbers())
+                    yield board_idx, winning_number * unmarked_sum
+
+
+def meta_solution(winning_numbers, boards):
+    scores = {board_idx: 0 for board_idx in range(0, len(boards))}
+    for board, score in solution(winning_numbers, boards):
+        # print(f'Board {board} is a winner! Score: {score} others: {scores.values()}')
+        if scores[board] == 0:
+            scores[board] = score
+        if all(scores.values()):
+            return score
+
+
+def read_board(lines):
+    board = []
+    for line in fd:
+        line = line.strip()
+        if line:
+            try:
+                integers = filter(lambda item: len(item), [item.strip() for item in line.split(' ')])
+                board_row = list(map(lambda x: int(x), integers))
+                board.append(board_row)
+            except ValueError:
+                raise ValueError(line, integers)
+        else:
+            return board
+
+
+def read_input(fd):
+    boards = []
+    board = []
+    winning_numbers = list(map(lambda x: int(x), fd.readline().split(',')))
+    fd.readline()
+    raw_boards = fd.readlines()
+    for line in raw_boards:
+        if line[0] != '\n':
+            integers = filter(lambda item: len(item), [item.strip() for item in line.split(' ')])
+            board_row = list(map(lambda x: int(x), integers))
+            board.append(board_row)
+        else:
+            boards.append(board)
+            board = []
+    if len(board):
+        boards.append(board)
+    # print('Boards: ', len(boards))
+    # print('Boards: ', boards)
+    return winning_numbers, boards
+
+
+if __name__ == '__main__':
+    with open('test_input.txt', 'r') as f:
+        data = read_input(f)
+    test_data_solution = meta_solution(*data)
+    assert test_data_solution == 1924, f'invalid response ({test_data_solution}) to test data'
+    data = read_input(sys.stdin)
+    print(meta_solution(*data))
